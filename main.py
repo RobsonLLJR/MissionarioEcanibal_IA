3ac301507b2d383a46ea06507243b8407e325494
Robson Liesner de Lima Junior <robsonlljr@gmail.com>
2021-05-11 20:16:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python
+
 class main():
     def __init__(self):
         self.fila = [Estados(3, 0, 3, 0, 'esquerda')]
         self.solucao = None
 
     def verificar_solucao(self):
+        for elemento in self.fila:
+            if elemento.verificar_final():
+                self.solucao[elemento]
+                while elemento.pai:
+                    self.solucao.insert(0, elemento.pai)
+                    elemento = elemento.pai
+                break;
 
 
 
@@ -22,13 +31,32 @@ class Estados():
         self.cani_esq = cani_esq
         self.cani_dir = cani_dir
         self.lado = lado
-
+        self.pai = None
+        self.filhos = []
 
     def verificar_final(self):
         valido_esq = self.mis_esq == self.cani_esq == 0
         valido_dir = self.mis_dir == self.cani_dir == 3
 
         return valido_esq and valido_dir
+
+
+    def gerar_filhos(self):
+        retorno_lado = 'dir' if self.lado == 'esq' else 'esq'
+
+        regrasBarco = [
+            {'missionarios': 2, 'canibais': 0},
+            {'missionarios': 1, 'canibais': 0},
+            {'missionarios': 1, 'canibais': 1},
+            {'missionarios': 0, 'canibais': 1},
+            {'missionarios': 0, 'canibais': 1},
+        ]
+
+        for movimento in regrasBarco:
+            if self.lado == 'esq':
+                newMiss_dir = self.mis_dir - movimento['missionarios']
+                newMiss_esq = self.mis_esq + movimento['missionarios']
+
 
 
 
